7c1054b405fc2b223b314d4556a9ad57490376ee
Sam Ritchie <sritchie09@gmail.com>
2011-12-01 11:39:24 -0800

    first commit .

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject cascading.kryo "0.1.0"
+  :description "Kryo serialization for Cascading."
+  :java-source-path "src/jvm"
+  :javac-options {:debug "true" :fork "true"}
+  :repositories {"conjars" "http://conjars.org/repo"}
+  :dependencies [[cascading/cascading-core "1.2.4"
+                  :exclusions [org.codehaus.janino/janino]]
+                 [com.googlecode/kryo "1.04"]
+                 [clojure "1.3.0"]]
+  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]])
---
+++ test/cascading/kryo/core_test.clj
@@ -0,0 +1,7 @@
+(ns cascading.kryo.core-test
+  (:use clojure.test
+        cascading.kryo.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
