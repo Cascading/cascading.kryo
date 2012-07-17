f18a9aa8cd810bd12ab3dc4333bfdbee427a0a1f
Sam Ritchie <sritchie09@gmail.com>
2012-07-17 16:17:45 -0700

    use output and input directly.

--- src/jvm/cascading/kryo/KryoDeserializer.java
+++ src/jvm/cascading/kryo/KryoDeserializer.java
@@ -4,7 +4,6 @@ import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import org.apache.hadoop.io.serializer.Deserializer;
 
-import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -14,9 +13,7 @@ public class KryoDeserializer implements Deserializer<Object> {
     private Kryo kryo;
     private final KryoSerialization kryoSerialization;
     private final Class<Object> klass;
-
-    private DataInputStream inputStream;
-    private Input input = null;
+    private Input input;
 
     public KryoDeserializer(KryoSerialization kryoSerialization, Class<Object> klass) {
         this.kryoSerialization =  kryoSerialization;
@@ -25,36 +22,20 @@ public class KryoDeserializer implements Deserializer<Object> {
 
     public void open(InputStream in) throws IOException {
         kryo = kryoSerialization.populatedKryo();
-
-        if( in instanceof DataInputStream)
-            inputStream = (DataInputStream) in;
-        else
-            inputStream = new DataInputStream( in );
+        input = new Input(in);
     }
 
     public Object deserialize(Object o) throws IOException {
-        int len = inputStream.readInt();
-        byte[] bytes = new byte[len];
-        inputStream.readFully( bytes );
-
-        if (input == null)
-            input = new Input(bytes);
-        else
-            input.setBuffer(bytes);
-
         return kryo.readObject(input, klass);
     }
 
     // TODO: Bump the kryo version, add a kryo.reset();
     public void close() throws IOException {
-        if( input != null )
-            input.close();
-
         try {
-            if( inputStream != null )
-                inputStream.close();
+            if( input != null )
+                input.close();
         } finally {
-            inputStream = null;
+            input = null;
         }
     }
 }
--- src/jvm/cascading/kryo/KryoSerializer.java
+++ src/jvm/cascading/kryo/KryoSerializer.java
@@ -4,27 +4,15 @@ import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Output;
 import org.apache.hadoop.io.serializer.Serializer;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
 /** User: sritchie Date: 12/1/11 Time: 11:57 AM */
 public class KryoSerializer implements Serializer<Object> {
 
-    /**
-     * Initial capacity of the Kryo Output object.
-     */
-    private static final int INIT_CAPACITY = 2000;
-
-    /**
-     * Maximum capacity of the Kryo Output object.
-     */
-    private static final int FINAL_CAPACITY = 2000000000;
-
     private Kryo kryo;
     private final KryoSerialization kryoSerialization;
-    private final Output output = new Output(INIT_CAPACITY, FINAL_CAPACITY);
-    private DataOutputStream outputStream;
+    private Output output;
 
     public KryoSerializer(KryoSerialization kryoSerialization) {
         this.kryoSerialization =  kryoSerialization;
@@ -32,32 +20,24 @@ public class KryoSerializer implements Serializer<Object> {
 
     public void open(OutputStream out) throws IOException {
         kryo = kryoSerialization.populatedKryo();
-
-        if( out instanceof DataOutputStream )
-            outputStream = (DataOutputStream) out;
-        else
-            outputStream = new DataOutputStream(out);
+        output = new Output(out);
     }
 
     public void serialize(Object o) throws IOException {
-        output.clear();
         kryo.writeObject(output, o);
-        byte[] bytes = output.toBytes();
-
-        outputStream.writeInt(bytes.length);
-        outputStream.write(bytes);
+        output.flush();
     }
 
     // TODO: Bump the kryo version, add a kryo.reset();
     public void close() throws IOException {
         kryo = null;
-        output.close();
 
         try {
-            if( outputStream != null )
-                outputStream.close();
+            if( output != null ) {
+                output.close();
+            }
         } finally {
-            outputStream = null;
+            output = null;
         }
     }
 }
