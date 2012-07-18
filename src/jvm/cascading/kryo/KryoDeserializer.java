920db8950d401da855fa93bf2ed46e966b68323e
Sam Ritchie <sritchie09@gmail.com>
2012-07-18 15:57:37 -0700

    Compensate for Kryo's stream greediness.

--- src/jvm/cascading/kryo/KryoDeserializer.java
+++ src/jvm/cascading/kryo/KryoDeserializer.java
@@ -21,12 +21,40 @@ public class KryoDeserializer implements Deserializer<Object> {
     }
 
     public void open(InputStream in) throws IOException {
+        if (!in.markSupported())
+            throw new IOException("InputStream must support `mark`.");
+
         kryo = kryoSerialization.populatedKryo();
         input = new Input(in);
     }
 
+    // As of version 2.16, Kryo overfills its internal buffer by slurping with wild
+    // abandon from its wrapped input stream. The following method compensates for this
+    // by resetting Input's position and limit after each slurp and moving the wrapped inputstream's
+    // pointer forward manually.
+
     public Object deserialize(Object o) throws IOException {
-        return kryo.readObject(input, klass);
+        // Marking phase.
+        InputStream inStream = input.getInputStream();
+        inStream.mark(KryoSerialization.MAX_OUTPUT_BUFFER_SIZE);
+        int startingPos = input.position();
+
+        // Reading phase.
+        Object obj = kryo.readObject(input, klass);
+        int finalPos = input.position();
+
+        // Skipping phase.
+        // inStream resets to the observed mark above. Resetting input's limit and position forces
+        // input to continue reading from its wrapped stream at the marked position.
+        inStream.reset();
+        input.setPosition(0);
+        input.setLimit(0);
+        int needToSkip = finalPos - startingPos;
+
+        while(needToSkip > 0)
+            needToSkip -= inStream.skip(needToSkip);
+
+        return obj;
     }
 
     // TODO: Bump the kryo version, add a kryo.reset();
--- src/jvm/cascading/kryo/KryoSerialization.java
+++ src/jvm/cascading/kryo/KryoSerialization.java
@@ -10,6 +10,9 @@ import org.objenesis.strategy.StdInstantiatorStrategy;
 
 /** User: sritchie Date: 12/1/11 Time: 11:43 AM */
 public class KryoSerialization extends Configured implements Serialization<Object> {
+    public static final int OUTPUT_BUFFER_SIZE = 1<<12;
+    public static final int MAX_OUTPUT_BUFFER_SIZE = 1<<24;
+
     Kryo kryo;
     KryoFactory factory;
 
