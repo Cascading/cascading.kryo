3a509fc4238c5b576e91c51b75e1969a46bd19cb
Rob Hall <rjhall@etsy.com>
2012-12-21 19:59:29 +0000

    Clean up

--- src/jvm/cascading/kryo/KryoDeserializer.java
+++ src/jvm/cascading/kryo/KryoDeserializer.java
@@ -10,40 +10,38 @@ import java.io.InputStream;
 
 public class KryoDeserializer implements Deserializer<Object> {
 
-    private java.io.DataInputStream is;
-    private Class<Object> klass;
-    private KryoSerialization ks;
-    private Kryo kryo;
+  private java.io.DataInputStream is;
+  private Class<Object> klass;
+  private KryoSerialization ks;
+  private Kryo kryo;
 
-    public KryoDeserializer(KryoSerialization kryoSerialization, Class<Object> klass) {
-      this.klass = klass;
-      ks = kryoSerialization;
-    }
+  public KryoDeserializer(KryoSerialization kryoSerialization, Class<Object> klass) {
+    this.klass = klass;
+    ks = kryoSerialization;
+  }
 
-    public void open(InputStream in) throws IOException {
-        is = (java.io.DataInputStream)in;
-        kryo = ks.populatedKryo();
-    }
+  public void open(InputStream in) throws IOException {
+    is = (DataInputStream)in;
+    kryo = ks.populatedKryo();
+  }
 
-    public Object deserialize(Object o) throws IOException {
-      System.out.println("about to deserialize a " + klass.getName());
-      int sz = is.readInt();
-      System.out.println("sz " + sz); 
-      byte[] b  = new byte[sz];
-      is.read(b);
-      //return(new java.io.ObjectInputStream(new java.io.ByteArrayInputStream(b))).readObject();
-      return(kryo.readObject(new Input(b), klass));
-    }
+  public Object deserialize(Object o) throws IOException {
+    int sz = is.readInt();
+    byte[] b  = new byte[sz];
+    is.read(b);
+    //return(new java.io.ObjectInputStream(new java.io.ByteArrayInputStream(b))).readObject();
+    return(kryo.readObject(new Input(b), klass));
+  }
 
-    // TODO: Bump the kryo version, add a kryo.reset();
-    public void close() throws IOException {
-        try {
-            if(is != null) is.close();
-        } catch(Exception e ) {
-          e.printStackTrace();
-        } finally {
-            is = null;
-            kryo = null;
-        }
+  // TODO: Bump the kryo version, add a kryo.reset();
+  public void close() throws IOException {
+    try {
+      if(is != null) is.close();
+    } catch(Exception e ) {
+      e.printStackTrace();
+    } finally {
+      is = null;
+      kryo = null;
     }
+  }
 }
--- src/jvm/cascading/kryo/KryoSerializer.java
+++ src/jvm/cascading/kryo/KryoSerializer.java
@@ -4,66 +4,65 @@ import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Output;
 import org.apache.hadoop.io.serializer.Serializer;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
 public class KryoSerializer implements Serializer<Object> {
-    private java.io.DataOutputStream os;
-    private KryoSerialization ks;
-    private Kryo kryo;
+  private java.io.DataOutputStream os;
+  private KryoSerialization ks;
+  private Kryo kryo;
 
-    public KryoSerializer(KryoSerialization kryoSerialization) {
-      ks = kryoSerialization;
-    }
+  public KryoSerializer(KryoSerialization kryoSerialization) {
+    ks = kryoSerialization;
+  }
 
-    public void open(OutputStream out) throws IOException {
-        os = (java.io.DataOutputStream)out;
-        kryo = ks.populatedKryo();
-    }
+  public void open(OutputStream out) throws IOException {
+    os = (java.io.DataOutputStream)out;
+    kryo = ks.populatedKryo();
+  }
 
-    public void serialize(Object o) throws IOException {
-      System.out.println("about to serialize a " + o.getClass().getName());
-      // get siez in bytes of serialized form.
-      FakeOutputStream fo = new FakeOutputStream();
-      //(new java.io.ObjectOutputStream(fo)).writeObject(o);
-      Output ko = new Output(fo);
-      kryo.writeObject(ko, o);
-      ko.flush();
-      int size = fo.size;
-      System.out.println("size " + size + " bytes");
-      java.io.ByteArrayOutputStream bho = new java.io.ByteArrayOutputStream(size);
-      //(new java.io.ObjectOutputStream(bho)).writeObject(o);
-      ko = new Output(bho);
-      kryo.writeObject(ko, o);
-      ko.flush();
-      os.writeInt(bho.size());
-      os.write(bho.toByteArray(), 0, bho.size());
-      os.flush();
-    }
+  public void serialize(Object o) throws IOException {
+    // get size in bytes of serialized form.
+    FakeOutputStream fo = new FakeOutputStream();
+    //(new java.io.ObjectOutputStream(fo)).writeObject(o);
+    Output ko = new Output(fo);
+    kryo.writeObject(ko, o);
+    ko.flush();
+    int size = fo.size;
+    ByteArrayOutputStream bho = new ByteArrayOutputStream(size);
+    //(new java.io.ObjectOutputStream(bho)).writeObject(o);
+    ko = new Output(bho);
+    kryo.writeObject(ko, o);
+    ko.flush();
+    os.writeInt(bho.size());
+    os.write(bho.toByteArray(), 0, bho.size());
+    os.flush();
+  }
 
-    // TODO: Bump the kryo version, add a kryo.reset();
-    public void close() throws IOException {
-        try {
-            if(os != null)
-              os.close();
-        } catch(Exception e) {
-            e.printStackTrace();
-        } finally {
-            os = null;
-            kryo = null;
-        }
+  // TODO: Bump the kryo version, add a kryo.reset();
+  public void close() throws IOException {
+    try {
+      if(os != null)
+        os.close();
+    } catch(Exception e) {
+      e.printStackTrace();
+    } finally {
+      os = null;
+      kryo = null;
     }
+  }
 
-    static class FakeOutputStream extends java.io.OutputStream {
-      int size = 0;
-      public void write(int b) {
-        size += 4;
-      }
-      public void write(byte[] b) {
-        size += b.length;
-      }
-      public void write(byte[] b, int off, int len) {
-        size += len;
-      }
+  static class FakeOutputStream extends OutputStream {
+    int size = 0;
+    public void write(int b) {
+      size += 4;
     }
+    public void write(byte[] b) {
+      size += b.length;
+    }
+    public void write(byte[] b, int off, int len) {
+      size += len;
+    }
+  }
 }
